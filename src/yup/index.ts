768cd3d153453b20a9d099f973c333be6f5c95e3
wand2016 <d19921207@gmail.com>
2023-11-19 14:34:47 +0900

    extracted ObjectTypeDefinition

--- src/yup/index.ts
+++ src/yup/index.ts
@@ -1,19 +1,8 @@
-import { indent } from '@graphql-codegen/visitor-plugin-common';
-import {
-  FieldDefinitionNode,
-  GraphQLSchema,
-  InputValueDefinitionNode,
-  NameNode,
-  ObjectTypeDefinitionNode,
-  TypeNode,
-} from 'graphql';
+import { GraphQLSchema } from 'graphql';
 
 import { ValidationSchemaPluginConfig } from '../config';
-import { buildApi, GeneratedCodesForDirectives } from '../directive';
 import { Interpreter, NewVisitor } from '../types';
-import { Visitor } from '../visitor';
 import { VisitorFactory } from '../VisitorFactory';
-import { isInput, isListType, isNamedType, isNonNullType } from './../graphql';
 import { ExportTypeStrategy } from './exportTypeStrategies/ExportTypeStrategy';
 import { createExportTypeStrategy } from './exportTypeStrategies/factory';
 import { ImportBuilder } from './ImportBuilder';
@@ -23,6 +12,7 @@ import { EnumTypeDefinitionFactory } from './visitFunctionFactories/EnumTypeDefi
 import { InputObjectTypeDefinitionFactory } from './visitFunctionFactories/InputObjectTypeDefinitionFactory';
 import { UnionTypesDefinitionFactory } from './visitFunctionFactories/UnionTypesDefinitionFactory';
 import { createWithObjectTypesSpec } from './withObjectTypesSpecs/factory';
+import { ObjectTypeDefinitionFactory } from './withObjectTypesSpecs/ObjectTypeDefinitionFactory';
 import { WithObjectTypesSpec } from './withObjectTypesSpecs/WithObjectTypesSpec';
 
 export class YupSchemaVisitor implements NewVisitor, Interpreter {
@@ -33,13 +23,11 @@ export class YupSchemaVisitor implements NewVisitor, Interpreter {
   private initialEmitter: InitialEmitter;
   private withObjectTypesSpec: WithObjectTypesSpec;
   private readonly inputObjectTypeDefinitionFactory: InputObjectTypeDefinitionFactory;
+  private readonly objectTypeDefinitionFactory: ObjectTypeDefinitionFactory;
   private readonly enumTypeDefinitionFactory: EnumTypeDefinitionFactory;
   private readonly unionTypesDefinitionFactory: UnionTypesDefinitionFactory;
 
-  constructor(
-    schema: GraphQLSchema,
-    private readonly config: ValidationSchemaPluginConfig
-  ) {
+  constructor(schema: GraphQLSchema, config: ValidationSchemaPluginConfig) {
     this.registry = new Registry();
     this.visitorFactory = new VisitorFactory(schema, config);
     this.exportTypeStrategy = createExportTypeStrategy(config.validationSchemaExportType);
@@ -50,6 +38,13 @@ export class YupSchemaVisitor implements NewVisitor, Interpreter {
       config,
       this.registry,
       this.visitorFactory
+    );
+    this.objectTypeDefinitionFactory = new ObjectTypeDefinitionFactory(
+      config,
+      this.registry,
+      this.visitorFactory.createVisitor('output'),
+      this.withObjectTypesSpec,
+      this.exportTypeStrategy
     );
     this.enumTypeDefinitionFactory = new EnumTypeDefinitionFactory(config, this.registry, this.visitorFactory);
     this.unionTypesDefinitionFactory = new UnionTypesDefinitionFactory(
@@ -76,31 +71,7 @@ export class YupSchemaVisitor implements NewVisitor, Interpreter {
 
   get ObjectTypeDefinition() {
     return {
-      leave: (node: ObjectTypeDefinitionNode) => {
-        if (!this.withObjectTypesSpec.shouldUseObjectTypeDefinitionNode(node)) return;
-
-        const visitor = this.visitorFactory.createVisitor('output');
-        const name = visitor.convertName(node.name.value);
-        this.registry.registerType(name);
-
-        // Building schema for field arguments.
-        const argumentBlocks = visitor.buildArgumentsSchemaBlock(node, (typeName, field) => {
-          this.registry.registerType(typeName);
-          return this.buildInputFields(field.arguments ?? [], visitor, typeName);
-        });
-        const appendArguments = argumentBlocks ? '\n' + argumentBlocks : '';
-
-        // Building schema for fields.
-        const shape =
-          node.fields
-            ?.map(field => {
-              const fieldSchema = generateFieldYupSchema(this.config, visitor, field, 2);
-              return isNonNullType(field.type) ? fieldSchema : `${fieldSchema}.optional()`;
-            })
-            .join(',\n') ?? '';
-
-        return this.exportTypeStrategy.objectTypeDefinition(name, node.name.value, shape, appendArguments);
-      },
+      leave: this.objectTypeDefinitionFactory.create(),
     };
   }
 
@@ -115,114 +86,4 @@ export class YupSchemaVisitor implements NewVisitor, Interpreter {
       leave: this.unionTypesDefinitionFactory.create(),
     };
   }
-
-  protected buildInputFields(
-    fields: readonly (FieldDefinitionNode | InputValueDefinitionNode)[],
-    visitor: Visitor,
-    name: string
-  ) {
-    const shape = fields
-      ?.map(field => {
-        const fieldSchema = generateFieldYupSchema(this.config, visitor, field, 2);
-        return isNonNullType(field.type) ? fieldSchema : `${fieldSchema}.optional()`;
-      })
-      .join(',\n');
-
-    return this.exportTypeStrategy.buildInputFields(shape, name);
-  }
 }
-
-const generateFieldYupSchema = (
-  config: ValidationSchemaPluginConfig,
-  visitor: Visitor,
-  field: InputValueDefinitionNode | FieldDefinitionNode,
-  indentCount: number
-): string => {
-  const generatedCodesForDirectives = buildApi(
-    field.name.value,
-    config.rules ?? {},
-    config.ignoreRules ?? [],
-    field.directives ?? []
-  );
-  const gen = generateFieldTypeYupSchema(config, visitor, field.type, null, generatedCodesForDirectives);
-  return indent(`${field.name.value}: ${maybeLazy(config, field.type, gen)}`, indentCount);
-};
-
-const generateFieldTypeYupSchema = (
-  config: ValidationSchemaPluginConfig,
-  visitor: Visitor,
-  type: TypeNode,
-  parentType: TypeNode | null,
-  generatedCodesForDirectives: GeneratedCodesForDirectives
-): string => {
-  if (isListType(type)) {
-    const gen = generateFieldTypeYupSchema(config, visitor, type.type, type, generatedCodesForDirectives);
-    const nullable = !parentType || !isNonNullType(parentType);
-    // NOTE: 配列の中身は必ず defined (nullが混ざることはあってもundefinedは混ざらない)
-    return `yup.array(${maybeLazy(config, type.type, `${gen}.defined()`)})${generatedCodesForDirectives.rulesForArray}${
-      nullable ? '.nullable()' : '.defined()'
-    }`;
-  }
-  if (isNonNullType(type)) {
-    const gen = generateFieldTypeYupSchema(config, visitor, type.type, type, generatedCodesForDirectives);
-    return maybeLazy(config, type.type, gen);
-  }
-  if (isNamedType(type)) {
-    const gen = generateNameNodeYupSchema(config, visitor, type.name) + generatedCodesForDirectives.rules;
-    if (!!parentType && isNonNullType(parentType)) {
-      if (visitor.shouldEmitAsNotAllowEmptyString(type.name.value)) {
-        return `${gen}.defined().required()`;
-      }
-      return `${gen}.defined().nonNullable()`;
-    }
-    const typ = visitor.getType(type.name.value);
-    if (typ?.astNode?.kind === 'InputObjectTypeDefinition') {
-      return `${gen}`;
-    }
-    return `${gen}.nullable()`;
-  }
-  console.warn('unhandled type:', type);
-  return '';
-};
-
-const generateNameNodeYupSchema = (config: ValidationSchemaPluginConfig, visitor: Visitor, node: NameNode): string => {
-  const converter = visitor.getNameNodeConverter(node);
-
-  switch (converter?.targetKind) {
-    case 'InputObjectTypeDefinition':
-    case 'ObjectTypeDefinition':
-    case 'UnionTypeDefinition':
-    case 'EnumTypeDefinition':
-      return createExportTypeStrategy(config.validationSchemaExportType).schemaEvaluation(
-        `${converter.convertName()}Schema`,
-        converter?.targetKind
-      );
-    default:
-      return yup4Scalar(config, visitor, node.value);
-  }
-};
-
-const maybeLazy = (config: ValidationSchemaPluginConfig, type: TypeNode, schema: string): string => {
-  if (isNamedType(type) && isInput(type.name.value) && config.lazyTypes?.includes(type.name.value)) {
-    // https://github.com/jquense/yup/issues/1283#issuecomment-786559444
-    return `yup.lazy(() => ${schema})`;
-  }
-  return schema;
-};
-
-const yup4Scalar = (config: ValidationSchemaPluginConfig, visitor: Visitor, scalarName: string): string => {
-  if (config.scalarSchemas?.[scalarName]) {
-    return `${config.scalarSchemas[scalarName]}`;
-  }
-  const tsType = visitor.getScalarType(scalarName);
-  switch (tsType) {
-    case 'string':
-      return `yup.string()`;
-    case 'number':
-      return `yup.number()`;
-    case 'boolean':
-      return `yup.boolean()`;
-  }
-  console.warn('unhandled name:', scalarName);
-  return `yup.mixed()`;
-};
---
+++ src/yup/withObjectTypesSpecs/ObjectTypeDefinitionFactory.ts
@@ -0,0 +1,159 @@
+import { indent } from '@graphql-codegen/visitor-plugin-common';
+import { FieldDefinitionNode, InputValueDefinitionNode, NameNode, ObjectTypeDefinitionNode, TypeNode } from 'graphql';
+
+import { ValidationSchemaPluginConfig } from '../../config';
+import { buildApi, GeneratedCodesForDirectives } from '../../directive';
+import { isInput, isListType, isNamedType, isNonNullType } from '../../graphql';
+import { Visitor } from '../../visitor';
+import { ExportTypeStrategy } from '../exportTypeStrategies/ExportTypeStrategy';
+import { createExportTypeStrategy } from '../exportTypeStrategies/factory';
+import { Registry } from '../registry';
+import { VisitFunctionFactory } from '../visitFunctionFactories/types';
+import { WithObjectTypesSpec } from './WithObjectTypesSpec';
+
+export class ObjectTypeDefinitionFactory implements VisitFunctionFactory<ObjectTypeDefinitionNode> {
+  constructor(
+    private readonly config: ValidationSchemaPluginConfig,
+    private readonly registry: Registry,
+    private readonly visitor: Visitor,
+    private readonly withObjectTypesSpec: WithObjectTypesSpec,
+    private readonly exportTypeStrategy: ExportTypeStrategy
+  ) {}
+
+  create() {
+    return (node: ObjectTypeDefinitionNode) => {
+      if (!this.withObjectTypesSpec.shouldUseObjectTypeDefinitionNode(node)) return;
+
+      const name = this.visitor.convertName(node.name.value);
+      this.registry.registerType(name);
+
+      // Building schema for field arguments.
+      const argumentBlocks = this.visitor.buildArgumentsSchemaBlock(node, (typeName, field) => {
+        this.registry.registerType(typeName);
+        return this.buildInputFields(field.arguments ?? [], this.visitor, typeName);
+      });
+      const appendArguments = argumentBlocks ? '\n' + argumentBlocks : '';
+
+      // Building schema for fields.
+      const shape =
+        node.fields
+          ?.map(field => {
+            const fieldSchema = generateFieldYupSchema(this.config, this.visitor, field, 2);
+            return isNonNullType(field.type) ? fieldSchema : `${fieldSchema}.optional()`;
+          })
+          .join(',\n') ?? '';
+
+      return this.exportTypeStrategy.objectTypeDefinition(name, node.name.value, shape, appendArguments);
+    };
+  }
+
+  protected buildInputFields(
+    fields: readonly (FieldDefinitionNode | InputValueDefinitionNode)[],
+    visitor: Visitor,
+    name: string
+  ) {
+    const shape = fields
+      ?.map(field => {
+        const fieldSchema = generateFieldYupSchema(this.config, visitor, field, 2);
+        return isNonNullType(field.type) ? fieldSchema : `${fieldSchema}.optional()`;
+      })
+      .join(',\n');
+
+    return this.exportTypeStrategy.buildInputFields(shape, name);
+  }
+}
+
+const generateFieldYupSchema = (
+  config: ValidationSchemaPluginConfig,
+  visitor: Visitor,
+  field: InputValueDefinitionNode | FieldDefinitionNode,
+  indentCount: number
+): string => {
+  const generatedCodesForDirectives = buildApi(
+    field.name.value,
+    config.rules ?? {},
+    config.ignoreRules ?? [],
+    field.directives ?? []
+  );
+  const gen = generateFieldTypeYupSchema(config, visitor, field.type, null, generatedCodesForDirectives);
+  return indent(`${field.name.value}: ${maybeLazy(config, field.type, gen)}`, indentCount);
+};
+
+const generateFieldTypeYupSchema = (
+  config: ValidationSchemaPluginConfig,
+  visitor: Visitor,
+  type: TypeNode,
+  parentType: TypeNode | null,
+  generatedCodesForDirectives: GeneratedCodesForDirectives
+): string => {
+  if (isListType(type)) {
+    const gen = generateFieldTypeYupSchema(config, visitor, type.type, type, generatedCodesForDirectives);
+    const nullable = !parentType || !isNonNullType(parentType);
+    // NOTE: 配列の中身は必ず defined (nullが混ざることはあってもundefinedは混ざらない)
+    return `yup.array(${maybeLazy(config, type.type, `${gen}.defined()`)})${generatedCodesForDirectives.rulesForArray}${
+      nullable ? '.nullable()' : '.defined()'
+    }`;
+  }
+  if (isNonNullType(type)) {
+    const gen = generateFieldTypeYupSchema(config, visitor, type.type, type, generatedCodesForDirectives);
+    return maybeLazy(config, type.type, gen);
+  }
+  if (isNamedType(type)) {
+    const gen = generateNameNodeYupSchema(config, visitor, type.name) + generatedCodesForDirectives.rules;
+    if (!!parentType && isNonNullType(parentType)) {
+      if (visitor.shouldEmitAsNotAllowEmptyString(type.name.value)) {
+        return `${gen}.defined().required()`;
+      }
+      return `${gen}.defined().nonNullable()`;
+    }
+    const typ = visitor.getType(type.name.value);
+    if (typ?.astNode?.kind === 'InputObjectTypeDefinition') {
+      return `${gen}`;
+    }
+    return `${gen}.nullable()`;
+  }
+  console.warn('unhandled type:', type);
+  return '';
+};
+
+const generateNameNodeYupSchema = (config: ValidationSchemaPluginConfig, visitor: Visitor, node: NameNode): string => {
+  const converter = visitor.getNameNodeConverter(node);
+
+  switch (converter?.targetKind) {
+    case 'InputObjectTypeDefinition':
+    case 'ObjectTypeDefinition':
+    case 'UnionTypeDefinition':
+    case 'EnumTypeDefinition':
+      return createExportTypeStrategy(config.validationSchemaExportType).schemaEvaluation(
+        `${converter.convertName()}Schema`,
+        converter?.targetKind
+      );
+    default:
+      return yup4Scalar(config, visitor, node.value);
+  }
+};
+
+const maybeLazy = (config: ValidationSchemaPluginConfig, type: TypeNode, schema: string): string => {
+  if (isNamedType(type) && isInput(type.name.value) && config.lazyTypes?.includes(type.name.value)) {
+    // https://github.com/jquense/yup/issues/1283#issuecomment-786559444
+    return `yup.lazy(() => ${schema})`;
+  }
+  return schema;
+};
+
+const yup4Scalar = (config: ValidationSchemaPluginConfig, visitor: Visitor, scalarName: string): string => {
+  if (config.scalarSchemas?.[scalarName]) {
+    return `${config.scalarSchemas[scalarName]}`;
+  }
+  const tsType = visitor.getScalarType(scalarName);
+  switch (tsType) {
+    case 'string':
+      return `yup.string()`;
+    case 'number':
+      return `yup.number()`;
+    case 'boolean':
+      return `yup.boolean()`;
+  }
+  console.warn('unhandled name:', scalarName);
+  return `yup.mixed()`;
+};
